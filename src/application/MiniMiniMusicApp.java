3fc6b4dd96e05283bd9b0e9bddb7875651de1dba
Ryan Connor <ryanwc13@gmail.com>
2016-03-09 19:04:41 +0700

    start short song; one chord

--- src/application/MiniMiniMusicApp.java
+++ src/application/MiniMiniMusicApp.java
@@ -20,20 +20,51 @@ public class MiniMiniMusicApp {
 			
 			Track track = seq.createTrack();
 			
-			ShortMessage a = new ShortMessage();
-			// below says "start playing, channel 1 (piano), note 44 (middle C), 
-			// at velocity (howfast/hard press) 100"
-			a.setMessage(144, 1, 44, 100);
-			MidiEvent noteOn = new MidiEvent(a, 1); // says "do event a at beat 1"
-			track.add(noteOn); // add the noteOn event to the track
-			
-			ShortMessage b = new ShortMessage();
-			b.setMessage(128, 1, 44, 100);
-			MidiEvent noteOff = new MidiEvent(b, 16);
-			track.add(noteOff);
+			for (int i = 0; i < 4; i++) {
+				
+				ShortMessage lowNoteStart = new ShortMessage();
+				ShortMessage medNoteStart = new ShortMessage();
+				ShortMessage highNoteStart = new ShortMessage();
+				ShortMessage lowNoteEnd = new ShortMessage();
+				ShortMessage medNoteEnd = new ShortMessage();
+				ShortMessage highNoteEnd = new ShortMessage();
+
+				lowNoteStart.setMessage(144, 1, 44, 100);
+				medNoteStart.setMessage(144, 1, 60, 100);
+				
+				lowNoteEnd.setMessage(128, 1, 44, 100);
+				medNoteEnd.setMessage(128, 1, 60, 100);
+				
+				MidiEvent lowNoteOn = new MidiEvent(lowNoteStart, 1+(16*i));
+				MidiEvent medNoteOn = new MidiEvent(medNoteStart, 5+(16*i));
+				
+				MidiEvent lowNoteOff = new MidiEvent(lowNoteEnd, 32);
+				MidiEvent medNoteOff = new MidiEvent(medNoteEnd, 32);
+				
+				track.add(lowNoteOn);
+				track.add(medNoteOn);
+				track.add(lowNoteOff);
+				track.add(medNoteOff);
+				
+				if (i % 2 != 1) {
+					System.out.println("even measure");
+					
+					highNoteStart.setMessage(144, 1, 70, 100);
+					highNoteEnd.setMessage(128, 1, 70, 100);
+				}
+				else {
+					
+					highNoteStart.setMessage(144, 1, 68, 100);
+					highNoteEnd.setMessage(128, 1, 68, 100);
+				}
+				
+				MidiEvent highNoteOn = new MidiEvent(highNoteStart, 9+(16*i));
+				MidiEvent highNoteOff = new MidiEvent(highNoteEnd, 32+(16*i));
+				track.add(highNoteOn);
+				track.add(highNoteOff);
+			}
 			
 			player.setSequence(seq);
-			
 			player.start();
 		}
 		catch (Exception ex) {
