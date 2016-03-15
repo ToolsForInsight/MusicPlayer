536a25a11b1d2e87d997a88d99d58384a9c290c3
Ryan Connor <ryanwc13@gmail.com>
2016-03-15 18:27:58 +0700

    example interactivity

--- src/application/SimpleGUI1.java
+++ src/application/SimpleGUI1.java
@@ -1,20 +1,34 @@
 package application;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class SimpleGUI1 {
+public class SimpleGUI1 implements ActionListener {
+	
+	JButton button;
 
 	public static void main(String[] args) {
 		
+		SimpleGUI1 gui = new SimpleGUI1();
+		gui.go();
+	}
+	
+	public void go() {
+		
 		JFrame frame = new JFrame();
-		JButton button = new JButton("click me");
+		
+		button = new JButton("click me");
+		
+		button.addActionListener(this);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // quit app upon close window
-		
 		frame.getContentPane().add(button);
-		
 		frame.setSize(300,300);
-		
 		frame.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		
+		button.setText("I've been clicked!");
 	}
 }
