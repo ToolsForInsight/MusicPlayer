ce47c76bdfabca180d296fec9f6f67a385aa8eb8
Ryan Connor <ryanwc13@gmail.com>
2016-03-17 19:27:26 +0700

    add new button and change layout

--- src/application/SimpleGUI1.java
+++ src/application/SimpleGUI1.java
@@ -7,11 +7,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.*;
 
-public class SimpleGUI1 implements ActionListener {
+public class SimpleGUI1 {
 	
 	JFrame frame;
-	JButton button;
-	MyDrawPanel myPanel;
+	JLabel label;
 
 	public static void main(String[] args) {
 		
@@ -25,19 +24,39 @@ public class SimpleGUI1 implements ActionListener {
 		frame.setSize(500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // quit app upon close window
 		
-		button = new JButton("Click me to change the above color gradient");
-		frame.getContentPane().add(BorderLayout.SOUTH, button);
-		button.addActionListener(this);
+		JButton colorButton = new JButton("Change Color");
+		colorButton.addActionListener(new ColorListener());
 		
-		myPanel = new MyDrawPanel();
+		JButton labelButton = new JButton("Change Label");
+		labelButton.addActionListener(new LabelListener());
+		
+		label = new JLabel("I'm a label");
+		
+		MyDrawPanel myPanel = new MyDrawPanel();
+		
+		frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+		frame.getContentPane().add(BorderLayout.EAST, labelButton);
 		frame.getContentPane().add(BorderLayout.CENTER, myPanel);
+		frame.getContentPane().add(BorderLayout.WEST, label);
+		
 		
 		frame.setVisible(true);
 		frame.repaint();
 	}
 	
-	public void actionPerformed(ActionEvent event) {
+	class ColorListener implements ActionListener {
 		
-		frame.repaint();
+		public void actionPerformed(ActionEvent event) {
+			
+			frame.repaint();
+		}
+	}
+	
+	class LabelListener implements ActionListener {
+		
+		public void actionPerformed(ActionEvent event) {
+			
+			label.setText("Ouch!");
+		}
 	}
 }
